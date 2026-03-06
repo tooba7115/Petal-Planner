46bbc4c78200314865375164bcb4fc2c1fd0372f
tooba7115 <tooba8784@gmail.com>
2026-03-06 11:09:58 +0000

    Added a dropdown menu with logout and settings

--- static/app.js
+++ static/app.js
@@ -337,3 +337,18 @@ document.addEventListener("DOMContentLoaded", () => {
   applyTheme(savedTheme);
 });
 
+
+//DROPDOWN MENU
+function toggleMenu() {
+  const menu = document.getElementById("dropdownMenu");
+  menu.classList.toggle("show");
+}
+
+document.addEventListener("click", function(event) {
+  const menu = document.getElementById("dropdownMenu");
+  const button = document.querySelector(".hamburger-btn");
+
+  if (!button.contains(event.target) && !menu.contains(event.target)) {
+    menu.classList.remove("show");
+  }
+});
